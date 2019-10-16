42067ef51b4d3fc727cadd8a1d13ac248ca13b6f
Luis Silva <lfsilva2611@gmail.com>
2019-10-16 10:24:49 +0100

    devbox

--- README.md
+++ README.md
@@ -5,6 +5,4 @@
 You need to run `aws configure` in your jenkins machine so the aws credentials will be shared with the container
 
 To deploy that just follow the instruction on the makefile:
-```make build```
-```make start```
-```make deploy```
+`make build`, `make start` and `make deploy`
--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,17 +1,11 @@
 provider "aws" {
-  region = "${var.aws_region}"
+  region = "eu-west-2"
 }
 
-# main.tf
 resource "aws_iam_user" "test" {
   name = "test"
 }
 
-variable "aws_region" {
-  default = "eu-west-2"
-}
-
-# output.tf
 output "IAM_user" {
   value = "${aws_iam_user.test.arn}"
 }
